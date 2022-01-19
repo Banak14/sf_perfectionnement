38b885a503744ad110b4a05181e53c1b9fec3ba6
Kourouma Banawata <banawata@MacBook-Pro-de-Kourouma.home>
2022-01-19 03:36:10 +0100

    initalisation

--- src/Controller/Front/ArticleController.php
+++ src/Controller/Front/ArticleController.php
@@ -10,9 +10,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ArticleController extends AbstractController
 {
 
-
-    #[Route("articles", name: "article_list")]
-
     public function articleList(ArticleRepository $articleRepository)
     {
         $articles = $articleRepository->findAll();
@@ -20,18 +17,12 @@ class ArticleController extends AbstractController
         return $this->render("front/articles.html.twig", ['articles' => $articles]);
     }
 
-
-    #[Route("article/{id}", name: "article_show")]
-
     public function articleShow($id, ArticleRepository $articleRepository)
     {
         $article = $articleRepository->find($id);
 
         return $this->render("front/article.html.twig", ['article' => $article]);
     }
-
-
-    #[Route("search", name: "front_search")]
 
     public function frontSearch(Request $request, ArticleRepository $articleRepository)
     {
